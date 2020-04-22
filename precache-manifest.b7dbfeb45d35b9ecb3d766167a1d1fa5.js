c877f5e60e59377aef723c0788da54be80535b98
Asif <asifrmemon@gmail.com>
2020-04-22 14:47:01 -0700

    Updates

---
+++ precache-manifest.b7dbfeb45d35b9ecb3d766167a1d1fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a9368d85b98b9bc7a3f46b0e130912",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c6d70f712ddc189a2b05",
+    "url": "/monsters-rolodex/static/css/main.2e243d55.chunk.css"
+  },
+  {
+    "revision": "fbb95933edcdffcfa8ac",
+    "url": "/monsters-rolodex/static/js/2.1fc20c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1fc20c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d70f712ddc189a2b05",
+    "url": "/monsters-rolodex/static/js/main.ccb17dcc.chunk.js"
+  },
+  {
+    "revision": "2c857dfeeffbc817f8eb",
+    "url": "/monsters-rolodex/static/js/runtime-main.4efd7d37.js"
+  }
+]);
